4f8cd1f87a9c740c9b575228baebd55b0fd98e15
Ian Weaver <weaveric@gmail.com>
2026-03-25 15:00:35 -0700

    tests(tree printing): make less brittle

--- test/test-show.jl
+++ test/test-show.jl
@@ -1,27 +1,9 @@
 @testset "Show method for `ASDF.ASDFFile`" begin
     af = load("blue_upchan_gain.00000000.asdf")
 
-    @test sprint(show, MIME"text/plain"(), af) == """
-    blue_upchan_gain.00000000.asdf
-    ├─ 0::Int64
-    │  ├─ buffer::ASDF.NDArray | shape = [256]
-    │  ├─ dim_names::Vector{String} | shape = (1,)
-    │  └─ dish_index::ASDF.NDArray | shape = [2, 3]
-    └─ asdf/library::String
-       ├─ author::String | Erik Schnetter
-       ├─ homepage::String | https://github.com/eschnett/asdf-cxx
-       ├─ name::String | asdf-cxx
-       └─ version::String | 7.2.0
-    """
+    @test occursin("blue_upchan_gain.00000000.asdf\n├─", sprint(show, MIME"text/plain"(), af))
 
-    @test sprint(io -> ASDF.info(io, af; max_rows = 5)) == """
-    blue_upchan_gain.00000000.asdf
-    ├─ 0::Int64
-    │  ├─ buffer::ASDF.NDArray | shape = [256]
-    │  ├─ dim_names::Vector{String} | shape = (1,)
-    │  └─ dish_index::ASDF.NDArray | shape = [2, 3]
-      ⋮  (5) more rows
-    """
+    @test occursin("(5) more rows", sprint(io -> ASDF.info(io, af; max_rows = 5)))
 
     # I'm sure there's a better way to test this code path
     @test ASDF.info(af; max_rows = 5) == nothing
