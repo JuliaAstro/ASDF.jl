189ef833bf6a96ef066d04ce5f668c85dbcd48f0
Ian Weaver <weaveric@gmail.com>
2026-03-25 14:02:05 -0700

    tests(tree printing): fix + add more

--- test/test-show.jl
+++ test/test-show.jl
@@ -1,7 +1,7 @@
 @testset "Show method for `ASDF.ASDFFile`" begin
     af = load("blue_upchan_gain.00000000.asdf")
 
-    sprint(show, MIME"text/plain"(), af) == """
+    @test sprint(show, MIME"text/plain"(), af) == """
     test/blue_upchan_gain.00000000.asdf
     ├─ 0::Int64
     │  ├─ buffer::ASDF.NDArray | shape = [256]
@@ -12,5 +12,14 @@
        ├─ homepage::String | https://github.com/eschnett/asdf-cxx
        ├─ name::String | asdf-cxx
        └─ version::String | 7.2.0
+    """
+
+    @test sprint(io -> ASDF.info(io, af; max_rows = 5)) == """
+    test/blue_upchan_gain.00000000.asdf
+    ├─ 0::Int64
+    │  ├─ buffer::ASDF.NDArray | shape = [256]
+    │  ├─ dim_names::Vector{String} | shape = (1,)
+    │  └─ dish_index::ASDF.NDArray | shape = [2, 3]
+      ⋮  (5) more rows
     """
 end
